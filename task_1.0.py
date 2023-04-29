926845fb565bac5d7156030d75c941ea91eb1b15
Yaroslav <tarasdmb2008@mail.ru>
2023-04-29 08:45:12 +0500

    second task

--- task_1.0.py
+++ task_1.0.py
@@ -3,4 +3,9 @@
 # Формула для получения n-го члена прогрессии: an = a1 + (n-1) * d.
 # Каждое число вводится с новой строки.
 
+a1 = int(input())
+d = int(input())
+n = int(input())
+for i in range(n):
+print(a1 + i * d)
 
